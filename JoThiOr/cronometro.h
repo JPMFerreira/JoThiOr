602f7cd2efdb785888f7c396eb80eccff5bbd85d
JPMFerreira <51541561+JPMFerreira@users.noreply.github.com>
2019-06-10 09:36:02 -0300

    Add files via upload
    primeira tentativa de subir mudanças

---
+++ JoThiOr/cronometro.h
@@ -0,0 +1,35 @@
+﻿#ifndef CRONOMETRO_H
+#define CRONOMETRO_H
+
+#include <ctime>
+#include <vector>
+
+class Cronometro {
+  public:
+    // Cria um cronômetro zerado e parado.
+    Cronometro();
+    // Inicia a contagem do tempo desde o zero.
+    void Iniciar();
+    // Pausa a contagem do tempo, serio?.
+    void Pausar();
+    // Pausa e zera a contagem do tempo terceira tentativa.
+    void Zerar();
+    // Retorna quantos segundos foram acumulados pelo cronômetro.
+    double tempo();
+    // Registra e retorna o tempo (em segundos) até uma volta do cronômetro. 
+    // PRECONDIÇÃO: O cronômetro não está parado.
+    double volta();
+    // Retorna a *duração* de cada volta registrada. 
+    std::vector<double> voltas();
+ private:
+  // Indica se o cronômetro está parado ou contando o tempo.
+  bool pausado_;
+  // Momento em que o cronômetro começou a contar o tempo.
+  time_t inicio_;  
+  // Tempo decorrido até que o cronômetro foi pausado pela última vez.
+  double acumulado_;   
+  // Tempos de quando as voltas foram registradas.
+  std::vector<double> tempos_;  
+};
+
+#endif
